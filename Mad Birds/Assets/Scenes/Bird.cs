2a0dfc966d76eb60e4f4092145597111b2cd111c
yacine <yacineyacine31212@gmail.com>
2020-07-04 18:56:34 +0100

    finish dragging logic

--- Mad Birds/Assets/Scenes/Bird.cs
+++ Mad Birds/Assets/Scenes/Bird.cs
@@ -1,15 +1,36 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
+﻿using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
-    Vector3 _initilaPosition;
+    private Vector3 _initilaPosition;
+    private bool _birdWasLuncheded;
+    private float _timeSettingAround;
 
+    [SerializeField] private float _lunchPower = 500;
+    
     private void Awake()
     {
         _initilaPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if(_birdWasLuncheded && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSettingAround += Time.deltaTime;
+        }
+        if (transform.position.y >  10 ||
+            transform.position.y < -10 ||
+            transform.position.x >  10 ||
+            transform.position.x < -10 ||
+            _timeSettingAround > 3)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+        
     }
 
     private void OnMouseDown()
@@ -19,9 +40,12 @@ public class Bird : MonoBehaviour
 
     private void OnMouseUp()
     {
+        
         GetComponent<SpriteRenderer>().color = Color.white;
         Vector2 directiontoInitialPosition = _initilaPosition - transform.position;
-        GetComponent<Rigidbody2D>().AddForce(directiontoInitialPosition);
+        GetComponent<Rigidbody2D>().AddForce(directiontoInitialPosition * _lunchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLuncheded = true;
     }
 
     private void OnMouseDrag()
