e69cfb1c3056bffdea8b3db2c55a963c1027ca72
yacine <yacineyacine31212@gmail.com>
2020-07-07 21:57:05 +0100

    add enemy logic

---
+++ Mad Birds/Assets/Scenes/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+        if (bird != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null) {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5) {
+            Destroy(gameObject);
+            return;
+        }
+
+    }    
+}
